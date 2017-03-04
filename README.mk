fd645a36ac9f6212ef3ee739ce632973cfa93dd1
Tim <tjschillinger92>
2017-03-04 14:29:39 -0700

    general adjustments and addition of readme file

---
+++ README.mk
@@ -0,0 +1,6 @@
+Tim's Portfolio Page
+==================
+The purpose of this Portfolio site is to showcase the work I have accomplished as a Designer and Front End Web Developer.
+
+
+![Landing Page](http://i.imgur.com/IOeCglw.png)
