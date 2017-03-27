86afed4811646515534a8319715c9794bfcc83e7
Tim <tjschillinger92>
2017-03-27 14:04:31 -0600

    bells and whistles and contact form

--- scripts/home.js
+++ scripts/home.js
@@ -4,3 +4,40 @@ $(".projects-scroll").click(function() {
     }, 1000);
 });
 
+$(document).ready(function() {
+      $('.delay').hide().delay(3000).fadeIn(1000);
+});
+
+$(document).ready(function () {
+    $(".popup").hide();
+    $(".openpop").click(function (e) {
+        e.preventDefault();
+        $("iframe").attr("src", $(this).attr('href'));
+        $(".links").fadeOut('slow');
+        $(".popup").fadeIn('slow');
+    });
+
+    $(".close").click(function () {
+        $(this).parent().fadeOut("slow");
+        $(".links").fadeIn("slow");
+    });
+});
+
+window.onload=function(){
+    $("#popup").on('click', function(){
+          $('.popup').bPopup({
+            content:'iframe', //'ajax', 'iframe' or 'image'
+            contentContainer:'.content',
+            modalColor: '#105A6A'
+        }); 
+    })
+    
+    $("#contact-popup").on('click', function(){
+          $('.popup').bPopup({
+            content:'iframe', //'ajax', 'iframe' or 'image'
+            contentContainer:'.content',
+            modalColor: '#105A6A'
+        }); 
+    })
+}
+
